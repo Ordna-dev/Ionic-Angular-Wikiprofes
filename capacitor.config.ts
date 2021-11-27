11fe9e5c7d3163c3738ab591ca7045faf16bb7eb
Alex <ordnajelaorerreug@gmail.com>
2021-11-26 20:13:45 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic-Wikiprofes2.0',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
